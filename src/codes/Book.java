b3e6f2cb2f77bbc24cbc5a5584f9762e02c6bcc3
clearsky77 <clearsky3264@gmail.com>
2021-11-12 10:38:01 +0900

    책 정보 출력 기능 작성

--- src/codes/Book.java
+++ src/codes/Book.java
@@ -4,4 +4,18 @@ public class Book {
 	String title;
 	int limitAge;
 	int rentFee;
+	
+	void printBookInfo() {
+		System.out.println("=== 도서 정보 ===");
+		System.out.println("제목: "+title);
+		
+		if(limitAge == 0) {
+			System.out.println("연령 제한: 전체 이용가");
+		}else {
+			System.out.println("연령 제한: "+limitAge+"세 이용가");
+		}
+		
+		System.out.println("대여료: "+rentFee);
+		System.out.println("==============");
+	}
 }
