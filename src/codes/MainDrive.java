2e0fe0ba580a18935e61e65dbde1e30380966d7b
clearsky77 <clearsky3264@gmail.com>
2021-11-12 10:49:50 +0900

    책 객체 생성

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -3,7 +3,15 @@ package codes;
 public class MainDrive {
 
 	public static void main(String[] args) {
-
+		Book b1 = new Book();
+		
+		b1.title = "어린 왕자";
+		b1.limitAge = 0;
+		b1.rentFee = 500;
+		
+		b1.title = "타짜";
+		b1.limitAge = 19;
+		b1.rentFee = 700;
 	}
 
 }
